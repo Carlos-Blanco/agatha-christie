71bbcafd1c1e659a8e3fb8aeaf13ca8eec5b7629
Carlos Blanco <carlosblanco@me.com>
2021-01-29 23:15:47 +0100

    Adding Profile View

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import BookDetail from "../views/BookDetail.vue";
 import Home from "../views/Home.vue";
 import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
+import Profile from "../views/Profile.vue";
 
 const routes = [
   {
@@ -25,6 +26,11 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile
   }
 ];
 
---
+++ src/views/Profile.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="profile">
+    <h2>Profile</h2>
+    <span id="errorMessage" class="error" style="display:none;"></span>
+    <button @click="logout">Log out</button>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+require("firebase/auth");
+
+export default {
+  name: "Login",
+  methods: {
+    logout() {
+      firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log("Log Out")
+      }).catch((error) => {
+        var errorMessage = error.message;
+        var logoutError = document.getElementById("errorMessage");
+        logoutError.style.display = "block";
+        logoutError.textContent += errorMessage;
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.profile {
+  margin: 1rem;
+  button {
+    height: 3rem;
+    background: #008661;
+    border-radius: var(--border-radius);
+    border: none;
+    color: white;
+    font-size: 1.2rem;
+    width: 100%;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  .error {
+    background: #ff5e58;
+    color: white;
+    min-height: 3rem;
+    border-radius: var(--border-radius);
+    padding: 0.7rem 1rem;
+    margin-bottom: 1rem;
+  }
+}
+</style>
