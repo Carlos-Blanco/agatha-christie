2a0fef56ffb287509ab577a6b474f4f6ffa18d10
Carlos Blanco <carlosblanco@me.com>
2021-02-03 21:01:15 +0100

    Removing items from books array

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,14 @@ export default createStore({
   },
   mutations: {
     ADD_BOOK_TO_COLLECTION(state, value){
-      if (state.readBooks.includes(value) === false) state.readBooks.push(value);
+      if (state.readBooks.includes(value)){
+        const index = state.readBooks.indexOf(value);
+        if (index > -1) {
+          state.readBooks.splice(index, 1);
+        }
+      } else {
+        state.readBooks.push(value);
+      }
     }
   },
   actions: {},
