0fd88b0a3863ba7633f38cffc9a0924ba7466758
Carlos Blanco <carlosblanco@me.com>
2021-04-08 19:49:03 +0200

    Adding NotFound component

---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Oops!!</h1>
+    <h3>The page you're looking for is not here.</h3>
+    <router-link :to="{ name: 'Home' }" class="btn">Back</router-link>
+  </div>
+</template>
